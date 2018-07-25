d4e5959331e97ec7e5fb33457a734199ca7cd17d
Egor Safronov <heysafronov@gmail.com>
2018-07-25 19:46:43 +0300

    updated data API

--- src/api/data.js
+++ src/api/data.js
@@ -64,5 +64,104 @@ export const normalizedTasks = [
     comments: "1",
     attach: "3",
     style: "card-color-low"
+  },
+  {
+    id: "f77gjfek",
+    type: "progress",
+    priority: "Med Priority",
+    user: "C. Guvert",
+    text: "Account profile flow diagrams",
+    avatar: "./3bc84a401a51991f895ac6f6f40b7010.jpg",
+    comments: "1",
+    attach: "2",
+    style: "card-color-med"
+  },
+  {
+    id: "f88gjfek",
+    type: "progress",
+    priority: "Low Priority",
+    user: "A. Nilson",
+    text: "Slide templates for client pitch project",
+    avatar: "./615f6d8539dbe37bc2c8f3d7d749182c.jpg",
+    comments: "3",
+    attach: "3",
+    style: "card-color-low"
+  },
+  {
+    id: "f99gjfek",
+    type: "progress",
+    priority: "Low Priority",
+    user: "M. Thompson",
+    text: "Review administrator console designs",
+    avatar: "./41aad055f35eb28f42b84ca1b4cf5d53.jpg",
+    comments: "2",
+    attach: "3",
+    style: "card-color-low"
+  },
+  {
+    id: "fuugjfek",
+    type: "review",
+    priority: "High Priority",
+    user: "A. Nilson",
+    text: "Social media posts",
+    avatar: "./615f6d8539dbe37bc2c8f3d7d749182c.jpg",
+    comments: "3",
+    attach: "2",
+    style: "card-color-high"
+  },
+  {
+    id: "d4lgj5ek",
+    type: "review",
+    priority: "Low Priority",
+    user: "V. Cobain",
+    text: "Shopping cart and product catalog wireframes",
+    avatar: "./0cafaf103d2eef926eebb15b20651c88.jpg",
+    comments: "1",
+    attach: "3",
+    style: "card-color-low"
+  },
+  {
+    id: "f77gjf6k",
+    type: "review",
+    priority: "Med Priority",
+    user: "C. Guvert",
+    text: "End user flow charts",
+    avatar: "./3bc84a401a51991f895ac6f6f40b7010.jpg",
+    comments: "2",
+    attach: "2",
+    style: "card-color-med"
+  },
+  {
+    id: "f9gjfek3",
+    type: "complete",
+    priority: "Med Priority",
+    user: "M. Thompson",
+    text: "Navigation designs",
+    avatar: "./41aad055f35eb28f42b84ca1b4cf5d53.jpg",
+    comments: "2",
+    attach: "3",
+    style: "card-color-med"
+  },
+  {
+    id: "f47gjf6k",
+    type: "complete",
+    priority: "Low Priority",
+    user: "C. Guvert",
+    text: "User profile prototypes",
+    avatar: "./3bc84a401a51991f895ac6f6f40b7010.jpg",
+    comments: "3",
+    attach: "2",
+    style: "card-color-low"
+  },
+  {
+    id: "d4lg23ek",
+    type: "complete",
+    priority: "High Priority",
+    user: "V. Cobain",
+    text: "Create style guide based on previous feedback",
+    avatar: "./0cafaf103d2eef926eebb15b20651c88.jpg",
+    comments: "2",
+    attach: "3",
+    style: "card-color-high"
   }
 ];
