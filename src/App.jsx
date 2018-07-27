d9ff759244223e4e28971ed9c6c7045778d9ff90
Egor Safronov <heysafronov@gmail.com>
2018-07-27 17:35:04 +0300

    added router build / added Loadable

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
 import "./index.css";
 import React from "react";
+import Loadable from "react-loadable";
 import { Route, Switch } from "react-router-dom";
-import Boards from "./components/Routers/Boards/index.jsx";
-import Manage from "./components/Routers/Manage/index.jsx";
-import Reports from "./components/Routers/Reports/index.jsx";
-import Schedule from "./components/Routers/Schedule/index.jsx";
-import Settings from "./components/Routers/Settings/index.jsx";
+
+const Loading = () => <div>Loading...</div>;
+
+const Manage = Loadable({
+  loader: () => import("./components/Routers/Manage/index.jsx"),
+  loading: Loading
+});
+
+const Boards = Loadable({
+  loader: () => import("./components/Routers/Boards/index.jsx"),
+  loading: Loading
+});
+
+const Schedule = Loadable({
+  loader: () => import("./components/Routers/Schedule/index.jsx"),
+  loading: Loading
+});
+
+const Reports = Loadable({
+  loader: () => import("./components/Routers/Reports/index.jsx"),
+  loading: Loading
+});
+
+const Settings = Loadable({
+  loader: () => import("./components/Routers/Settings/index.jsx"),
+  loading: Loading
+});
+
+const Application = () => (
+  <Switch>
+    <Route path="/manage" component={Manage} />
+    <Route exact path="/" component={Boards} />
+    <Route exact path="/schedule" component={Schedule} />
+    <Route path="/reports" component={Reports} />
+    <Route path="/settings" component={Settings} />
+  </Switch>
+);
 
 class App extends React.Component {
   render() {
-    return (
-      <Switch>
-        <Route path="/manage" component={Manage} />
-        <Route exact path="/" component={Boards} />
-        <Route exact path="/schedule" component={Schedule} />
-        <Route path="/reports" component={Reports} />
-        <Route path="/settings" component={Settings} />
-      </Switch>
-    );
+    return <Application />;
   }
 }
 
