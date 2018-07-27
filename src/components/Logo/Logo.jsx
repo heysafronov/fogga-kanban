b2f521f3b7d0ada677842d3e96c7f112df2c07b9
Egor Safronov <heysafronov@gmail.com>
2018-07-27 22:51:09 +0300

    added HOC dragging

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Logo extends React.Component {
   render() {
     return (
-      <section className="kanban__logo">
-        <a href="/">
+      <Link to="/">
+        <section className="kanban__logo">
           <span>fogga</span>
-        </a>
-      </section>
+        </section>
+      </Link>
     );
   }
 }
