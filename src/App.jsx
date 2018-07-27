43b1ad24446cffcdc2b1df720b01f49f96baa5fa
Egor Safronov <heysafronov@gmail.com>
2018-07-27 19:55:41 +0300

    deleted source-map

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,30 @@ import "./index.css";
 import React from "react";
 import Loadable from "react-loadable";
 import { Route, Switch } from "react-router-dom";
-
 const Loading = () => <div>Loading...</div>;
 
 const Manage = Loadable({
-  loader: () => import("./components/Routers/Manage/index.jsx"),
+  loader: () => import("./components/Routes/Manage/index.jsx"),
   loading: Loading
 });
 
 const Boards = Loadable({
-  loader: () => import("./components/Routers/Boards/index.jsx"),
+  loader: () => import("./components/Routes/Boards/index.jsx"),
   loading: Loading
 });
 
 const Schedule = Loadable({
-  loader: () => import("./components/Routers/Schedule/index.jsx"),
+  loader: () => import("./components/Routes/Schedule/index.jsx"),
   loading: Loading
 });
 
 const Reports = Loadable({
-  loader: () => import("./components/Routers/Reports/index.jsx"),
+  loader: () => import("./components/Routes/Reports/index.jsx"),
   loading: Loading
 });
 
 const Settings = Loadable({
-  loader: () => import("./components/Routers/Settings/index.jsx"),
+  loader: () => import("./components/Routes/Settings/index.jsx"),
   loading: Loading
 });
 
@@ -34,7 +33,7 @@ const Application = () => (
   <Switch>
     <Route path="/manage" component={Manage} />
     <Route exact path="/" component={Boards} />
-    <Route exact path="/schedule" component={Schedule} />
+    <Route path="/schedule" component={Schedule} />
     <Route path="/reports" component={Reports} />
     <Route path="/settings" component={Settings} />
   </Switch>
