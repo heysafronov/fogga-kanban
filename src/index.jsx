f931eabaaf3e69b311b1bb5eb54459fe03d31854
Egor Safronov <heysafronov@gmail.com>
2018-07-28 10:55:24 +0300

    edited import App

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./App.jsx";
+import App from "./app.jsx";
 import store from "./store";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
