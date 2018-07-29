bf29e1a0378815ec27d793e65976c1f017466123
Egor Safronov <heysafronov@gmail.com>
2018-07-29 15:15:58 +0300

    deleted Links for class Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 class Logo extends React.Component {
   render() {
     return (
-      <Link to="/">
-        <section className="kanban__logo">
-          <span>fogga</span>
-        </section>
-      </Link>
+      <section className="kanban__logo">
+        <span>fogga</span>
+      </section>
     );
   }
 }
