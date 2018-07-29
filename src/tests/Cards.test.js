a2ba40e79d4d5e15ae8ee99e631c9d9bbe22e35d
Egor Safronov <heysafronov@gmail.com>
2018-07-29 12:18:31 +0300

    added test: should render a list of cards

--- src/tests/Cards.test.js
+++ src/tests/Cards.test.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Enzyme, { shallow, mount, render } from "enzyme";
+import store from "../store/";
+import Enzyme, { mount } from "enzyme";
+import { Provider } from "react-redux";
 import Adapter from "enzyme-adapter-react-16";
 import { normalizedTasks } from "../api/data";
 import Cards from "../components/Main/Cards.jsx";
-import { Provider } from "react-redux";
-import store from "../store/";
 
 Enzyme.configure({ adapter: new Adapter() });
 
